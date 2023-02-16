075e649eb2610174915b7f389688f51210247621
CyprianEsogol <reececyprian@gmail.com>
2023-02-16 14:02:21 -0800

    unix operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - A c program that uses the write function to print and returns a 1
+ *Return: Always 1
+ *
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
