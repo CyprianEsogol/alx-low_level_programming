d603cf37627cd5a5c58b1e906f2a4eae064a679c
CyprianEsogol <reececyprian@gmail.com>
2023-02-27 04:38:08 -0800

    parameter and updates the value it points

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer ro an int as a 
+ * parameterand updates the value it points to 98
+ * @*n: parameter that updates the value it points to 98
+ * Return: (0)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
