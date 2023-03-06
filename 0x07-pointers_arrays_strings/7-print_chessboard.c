e070b6cae1541daa72b2c3f87c7cc1050a49247f
CyprianEsogol <reececyprian@gmail.com>
2023-03-06 06:12:26 -0800

    function that prints the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,12 +10,10 @@ void print_chessboard(char (*a)[8])
 	int x;
 	int y;
 
-	x = 0;
-	while (x < 8)
+	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
 			_putchar(a[x][y]);
 		_putchar('\n');
 	}
-	x++;
 }
