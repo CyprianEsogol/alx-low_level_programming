b3796b3f40e86f2e1a93cea038aeb9b0887b3174
CyprianEsogol <reececyprian@gmail.com>
2023-03-22 11:15:48 -0700

    program that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the results
+ * @x: arguments
+ * @y: array
+ * Return: 0
+ */
+int main(int x, char *y[])
+{
+	int a;
+	int b;
+	char *c;
+
+	if (x != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(y[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	c = (char *)main;
+	b = 0;
+	while (b < a)
+	{
+		if (b == a - 1)
+		{
+			printf("%02hhx\n", c[b]);
+			break;
+		}
+		printf("%02hhx", c[b]);
+		b++;
+	}
+	return (0);
+}
